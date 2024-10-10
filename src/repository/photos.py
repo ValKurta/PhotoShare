d20cbd5501d0cc37d3bea228b267012cc83c7078
Aleksandr <aleksandr.voitushenko@gmail.com>
2024-10-10 10:46:58 +0200

    commit dev

---
+++ src/repository/photos.py
@@ -0,0 +1,68 @@
+from typing import List, Type
+from sqlalchemy import and_
+from sqlalchemy.orm import Session
+from src.database.models import Photo, User, Tag
+from src.schemas import PhotoModel, PhotoResponse, TagModel, TagsPhoto
+from datetime import date, timedelta
+
+from fastapi import UploadFile
+
+
+async def create_photo(text: str, url: str, db: Session) -> Photo:
+
+    photo = Photo(
+        user_id=1,
+        url=url,
+        description=text
+    )
+    db.add(photo)
+    db.commit()
+    db.refresh(photo)
+    return photo
+
+
+async def read_photo(photo_id: int, db: Session) -> Type[Photo] | None:
+    return db.query(Photo).filter(Photo.id == photo_id).first()
+
+
+async def update_photo(photo_id: int, url: UploadFile, description: str, db: Session) -> Photo | None:
+
+    photo = db.query(Photo).filter(Photo.id == photo_id).first()
+    if photo:
+        photo.url = url
+        photo.description = description
+        db.commit()
+    return photo
+
+
+async def delete_photo(photo_id: int, db: Session) -> Photo | None:
+
+    photo = db.query(Photo).filter(Photo.id == photo_id).first()
+    print(photo.url)
+    if photo:
+        db.delete(photo)
+        db.commit()
+    return photo
+
+
+async def change_description(photo_id: int, description: str, db: Session) -> Photo | None:
+
+    photo = db.query(Photo).filter(Photo.id == photo_id).first()
+    if photo:
+        photo.description = description
+        db.commit()
+    return photo
+
+
+async def add_tags(photo_id: int, body: TagsPhoto, db: Session) -> Photo | None:
+
+    photo = db.query(Photo).filter(Photo.id == photo_id).first()
+    if photo:
+        photo.tags = body.tags
+        db.commit()
+    return photo
+
+
+async def search_photos(tag: str, db: Session) -> list[Photo]:
+    photos = db.query(Photo).filter(tag in Photo.tags).all()
+    return list(photos)
---
+++ src/routes/photos.py
@@ -0,0 +1,88 @@
+import datetime
+import os
+import pickle
+
+from typing import List
+from fastapi import APIRouter, Depends, status, UploadFile, File, HTTPException, Form
+from sqlalchemy.orm import Session
+import cloudinary
+import cloudinary.uploader
+
+from src.database.db import get_db
+from src.repository import photos as repository_photos
+from src.schemas import PhotoResponse, PhotoModel, TagsPhoto
+from src.conf.config import settings
+
+router = APIRouter(prefix="/photos", tags=["photos"])
+
+
+@router.post('/', response_model=PhotoResponse)
+async def create_photo(file: UploadFile = File(),
+                       description: str = Form(),
+                       db: Session = Depends(get_db)):
+    cloudinary.config(
+        cloud_name=settings.cloudinary_name,
+        api_key=settings.cloudinary_api_key,
+        api_secret=settings.cloudinary_api_secret,
+        secure=True
+    )
+    r = cloudinary.uploader.upload(file.file,
+                                   public_id=f'PhotoShare/{description[:7].replace(" ", "") +
+                                                           file.filename.replace(".", "")}',
+                                   overwrite=True)
+    url = cloudinary.CloudinaryImage(f'PhotoShare/{description[:7].replace(" ", "") +
+                                                   file.filename.replace(".", "")}') \
+        .build_url(crop='fill', version=r.get('version'))
+
+    return await repository_photos.create_photo(description, url, db)
+
+
+@router.get('/{photo_id}', response_model=PhotoResponse)
+async def read_photo(photo_id: int, db: Session = Depends(get_db)):
+    return await repository_photos.read_photo(photo_id, db)
+
+
+@router.put('/{photo_id}', response_model=PhotoResponse)
+async def update_photo(photo_id: int,
+                       file: UploadFile = File(),
+                       description: str = Form(),
+                       db: Session = Depends(get_db)):
+    cloudinary.config(
+        cloud_name=settings.cloudinary_name,
+        api_key=settings.cloudinary_api_key,
+        api_secret=settings.cloudinary_api_secret,
+        secure=True
+    )
+    r = cloudinary.uploader.upload(file.file,
+                                   public_id=f'PhotoShare/{description[:7].replace(" ", "") +
+                                                           file.filename.replace(".", "")}',
+                                   overwrite=True)
+    url = cloudinary.CloudinaryImage(f'PhotoShare/{description[:7].replace(" ", "") +
+                                                   file.filename.replace(".", "")}') \
+        .build_url(crop='fill', version=r.get('version'))
+
+    photo = await repository_photos.update_photo(photo_id, url, description, db)
+    if photo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Contact not found')
+    return photo
+
+
+@router.delete('/{photo_id}', response_model=PhotoResponse)
+async def delete_photo(photo_id: int,
+                       db: Session = Depends(get_db)):
+
+    photo = await repository_photos.delete_photo(photo_id, db)
+    if photo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Contact not found')
+    return photo
+
+
+@router.patch('/{photo_id}', response_model=PhotoResponse)
+async def change_description(photo_id: int,
+                             description: str,
+                             db: Session = Depends(get_db)):
+
+    photo = await repository_photos.change_description(photo_id, description, db)
+    if photo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Contact not found')
+    return photo
