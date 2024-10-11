ce59b64b96fa49202bce145356cc6a8c8e522350
Alex <oleksii.uzhva.pz.2021@lpnu.ua>
2024-10-11 22:58:51 +0300

    feat: CRUD for admin + statistics v1

--- src/routes/admin_moderation_routes.py
+++
@@ -1,9 +0,0 @@
-# to check if user is admin
-from fastapi import APIRouter, Depends
-from src.services.auth import get_current_user
-from src.repository.token_blacklist import add_token_to_blacklist
-
-router = APIRouter()
-
-
-# Simply, when the user is admin, do everything you do in CRUD for a single usir, but for all users
